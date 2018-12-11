a0288bdb88da739ec918371ef5854d6327cabcca
Leo <serbert619@gmail.com>
2018-12-11 10:11:43 -0800

    first commit

---
+++ Time_Management.java
@@ -0,0 +1,49 @@
+
+public class Time_Management {
+	//need to make prompt for what if statements for each
+	public int hoursInADay = 24;{
+	}
+	public int work = 8;{
+		}
+	public int sleep = 9;{
+		
+	}
+	public int breakfast = 1; {
+		
+	}
+	public int lunch = 1;{
+		
+	}
+	public int dinner = 1;{
+		
+	}
+	public int videoGames = 2;{
+		
+	}
+	public int television = 2;{
+		
+	}
+	public int walkingDogs = 2;{
+		
+	}
+	public int dogPark = 3;{
+		
+	}
+	public int nap = 1;{
+		
+	}
+	public int homeWork = 1;{
+		
+	}
+	public int food = breakfast + lunch + dinner;{
+		
+	}
+	public double meals = (0.5*food);{
+		// meals generally take 30min. Why half of 1 hour is equal to 0.5 hours.
+	}
+	//Trying to figure out to calculate hours
+	public static void main(String[] args){
+		Time_Management schedule = new Time_management ();
+		System.out.println()
+	}
+}
